18f95739cd661600f4003e22da32f3505bb900bd
Anupat Kaewmee <anupat.k64@rsu.ac.th>
2024-04-15 15:02:54 +0700

    add get attractions

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import mysql from "mysql2";
 
 dotenv.config();
 
 const app = express();
 const PORT = parseInt(process.env.PORT) || 3000;
+const DB_URL = process.env.DB_URL;
+
+const conn = mysql.createConnection(DB_URL);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/attractions", (req, res) => {
+  conn.query("SELECT * FROM attractions", (err, results, fields) => {
+    res.send(results);
+  });
 });
 
 app.listen(PORT, () => {
